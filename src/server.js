3c19df74e6b6d765dfac3d47e1a652c13126363f
Aak172003 <prajapatiaakash364@gmail.com>
2024-12-12 00:13:33 +0530

    setup typescript for project

--- src/server.js
+++
@@ -1,2 +0,0 @@
-console.log("Welcome to Auth Service");
-console.log("Auth Server");
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+console.log("Welcome to Auth Service");
+console.log("Auth Server");
+
+// Here i specify name data type
+function welcome(name: string) {
+  console.log("welcome" + name);
+}
+
+welcome("Aakash");
