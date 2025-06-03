0e2d21e9ab84903d85a5f9668aa095de9909f1e9
Aak172003 <prajapatiaakash364@gmail.com>
2025-06-04 00:12:13 +0530

    setup eslint with typescript project and in eslint.config.mjs set rules

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    // ...tseslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        ignores: ["dist", "node_modules", "eslint.config.mjs"],
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            // no-console have 3 values : off, warn, error
+            // off : no error
+            // warn : warning
+            // error : error
+            "no-console": "off",
+            // dot-notation is used to check if the property is accessed using dot notation
+            "dot-notation": "error",
+        },
+    },
+);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,28 @@
 function welcomeMessage(name: string) {
+    const user = {
+        name: "Aakash",
+        age: 20,
+        email: "aakash@gmail.com",
+    };
+
+    // This give error because i defiend if to access keys inside object provide error
+    /*
+     9:22  error  ["name"] is better written in dot notation   dot-notation
+    10:22  error  ["age"] is better written in dot notation    dot-notation
+    11:22  error  ["email"] is better written in dot notation  dot-notation
+
+    This below is giver error 
+    console.log(user["name"]);
+    console.log(user["age"]);
+    console.log(user["email"]);
+    */
+    console.log(user.name);
+    console.log(user.age);
+    console.log(user.email);
+
     console.log(`Welcome to the Full Stack Development Project ${name}`);
 }
+
+console.log("Hello World");
 
 welcomeMessage("Aakash");
