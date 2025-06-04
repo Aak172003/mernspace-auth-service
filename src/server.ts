1d6ef89c8e27a06e59e0de02198c9eba8bf8cfa7
Aak172003 <prajapatiaakash364@gmail.com>
2025-06-04 14:32:16 +0530

    setup logger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { ConfigVariables } from "./config";
 import app from "./app";
+import logger from "./config/logger";
 
 console.log(ConfigVariables.PORT);
 
@@ -7,6 +8,11 @@ const startServer = () => {
     const PORT = ConfigVariables.PORT;
     try {
         app.listen(PORT, () => {
+            logger.info("Server Listening on port", { port: PORT });
+            logger.error("We found error ");
+
+            // This logger will not see because i choose info for console transport
+            logger.silly("Hi silly logger");
             console.log(`Listening on port ${PORT}`);
         });
     } catch (error) {
