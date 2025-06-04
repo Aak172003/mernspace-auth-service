72dfbc21745ebba8b2aaf5f24ad6fbe6c9ba261c
Aak172003 <prajapatiaakash364@gmail.com>
2025-06-04 13:13:17 +0530

    setup express server and appand sepearte both one for app related configuaration and other for server related

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,18 @@
 import { ConfigVariables } from "./config";
+import app from "./app";
 
 console.log(ConfigVariables.PORT);
+
+const startServer = () => {
+    const PORT = ConfigVariables.PORT;
+    try {
+        app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+startServer();
