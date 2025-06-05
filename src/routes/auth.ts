9605a4ab862d944e26a384fa2afa8adefe9f9ee5
Aak172003 <prajapatiaakash364@gmail.com>
2025-06-05 12:48:26 +0530

    write first test for user registration in better folder and file structure also add TDD Notes and enable verbose true to see running test cases in pro[er format

---
+++ src/routes/auth.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { AuthController } from "../controllers/AuthController";
+
+const router = express.Router();
+
+const authController = new AuthController();
+
+// Here bindiing issue occurs if we use authcontroller.register directly because this will not refer actual function , bevause here this refer this line context
+router.post("/register", (req, res) => {
+    authController.register(req, res);
+});
+export default router;
