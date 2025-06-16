1612711daf62cbdfce408452a2cfb26d54b85fbb
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-06-16 16:27:38 +0530

    setup-github-actions-ciyaml-file

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,11 +4,9 @@
 // This is one way to read all variables from .env files
 import { config } from "dotenv";
 import path from "path";
-
 config({
     path: path.join(__dirname, `../../.env.${process.env.NODE_ENV ?? "dev"}`),
 });
-
 const {
     PORT,
     NODE_ENV,
