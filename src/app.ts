1357d99b013d2b37cbcfd7e1e904617b4a9f35e3
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-06-19 22:06:38 +0530

    implement-cors-origin

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,6 @@ app.use(
     cors({
         // Here we basciaclly defines which domains can access my app server
         origin: ["http://localhost:5173"],
-
         credentials: true,
     }),
 );
